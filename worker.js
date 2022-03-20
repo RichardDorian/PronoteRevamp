eeb7b956d9f32e4800494fec23702e42a219e069
RichardDorian <richard.dorian@outlook.com>
2022-03-20 18:56:49 +0100

    feat: :sparkles: setup service worker

---
+++ worker.js
@@ -0,0 +1,12 @@
+// Make the service worker update instantly
+self.addEventListener('install', () => self.skipWaiting());
+
+/**
+ * Triggered when the fetch event is fired on the service worker
+ * @param {FetchEvent} event Event that triggered the function
+ */
+function fetchCallback(event) {
+  console.log(event);
+}
+
+self.addEventListener('fetch', fetchCallback);

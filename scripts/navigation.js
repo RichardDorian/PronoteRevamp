b5e3ca8223621a40a4438df7c1cc1886306bb284
RichardDorian <richard.dorian@outlook.com>
2022-03-23 14:39:51 +0100

    feat: :sparkles: handle back button

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,21 +1,3 @@
-/**
- * Show the given page to the screen while hiding the others
- * @param {HTMLElement} page HTMLElement of the page to show
- */
-function showPage(page) {
-  if (!page.classList.contains('page'))
-    return console.error("Can't show a non-page element");
-
-  for (const key in pages) {
-    /** @type {HTMLElement} */
-    const page = pages[key];
-    page.style.display = 'none';
-  }
-  page.style.display = 'block';
-
-  console.debug('Showing page', page);
-}
-
 const pages = {
   login: document.getElementById('login-page'),
   mainMenu: document.getElementById('main-menu-page'),
@@ -27,20 +9,67 @@ const pages = {
   teachers: document.getElementById('teachers-page'),
 };
 
+/**
+ * Show the given page to the screen while hiding the others
+ * @param {HTMLElement} page HTMLElement of the page to show
+ * @param {boolean} addToHistory Add new page to history
+ */
+function showPage(page, addToHistory = true) {
+  if (!page.classList.contains('page'))
+    return console.error("Can't show a non-page element");
+
+  for (const key in pages) {
+    /** @type {HTMLElement} */
+    const page = pages[key];
+    page.style.display = 'none';
+  }
+  page.style.display = 'block';
+
+  // Handling back button
+  if (addToHistory)
+    window.history.pushState(
+      {},
+      '',
+      page.id.includes('main-menu-page')
+        ? '/?page=mainMenu'
+        : `/?page=${page.id.replace('-page', '')}`
+    );
+  console.log(addToHistory);
+
+  console.debug('Showing page', page);
+}
+
 // Show the authentication form if the user is not authenticated
 if (!authenticated) {
   const loginPage = document.getElementById('login-page');
   loginPage.style.display = 'block';
   console.debug('User is not authenticated, showing login page');
 } else {
-  showPage(pages.mainMenu);
+  showPage(pages.mainMenu, false);
+}
+
+/**
+ * Show the page corresponding to the current URL query
+ * @param {boolean} addToHistory Add new page to history
+ */
+function showQueryPage(addToHistory = true) {
+  const parsedQuery = new URLSearchParams(window.location.search);
+  const pageQuery = parsedQuery.get('page');
+  if (pageQuery && authenticated) {
+    const page = pages[pageQuery];
+    if (page) showPage(page, addToHistory);
+    else console.warn('Unknown page query', pageQuery);
+  } else if (authenticated) showPage(pages.mainMenu, false);
 }
 
 // Show screen corresponding to the query string
-const parsedQuery = new URLSearchParams(window.location.search);
-const pageQuery = parsedQuery.get('page');
-if (pageQuery && authenticated) {
-  const page = pages[pageQuery];
-  if (page) showPage(page);
-  else console.warn('Unknown page query', pageQuery);
-}
+// Not adding to history by default because the browser already do it for us
+showQueryPage(false);
+
+/**
+ * Triggered when user is pressing the back button while without leaving the app
+ * @param {PopStateEvent} event Event
+ */
+window.onpopstate = (event) => {
+  showQueryPage(false);
+};

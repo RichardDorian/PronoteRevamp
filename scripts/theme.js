3885cabb1dd3373840993be129f68d93b70688cf
RichardDorian <richard.dorian@outlook.com>
2022-03-24 09:07:24 +0100

    fix: :bug: fix `rgbToHex` method for theme-color

--- scripts/theme.js
+++ scripts/theme.js
@@ -44,7 +44,7 @@ function applyTheme(theme = pageTheme, saveToStorage = false) {
    * @returns {string} Hex color
    */
   function rgbToHex(r, g, b) {
-    return `"${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
     // return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
 

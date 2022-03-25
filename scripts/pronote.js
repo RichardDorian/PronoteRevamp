a5e85ebd2ab1ffdad071550fe82d3d929dbd74a8
RichardDorian <richard.dorian@outlook.com>
2022-03-25 15:20:44 +0100

    chore: :construction: add pronote typedef

--- scripts/pronote.js
+++ scripts/pronote.js
@@ -7,7 +7,7 @@
  */
 
 /**
- * @typedef {Object} Lesson Lesson object
+ * @typedef {Object} Lesson
  * @property {Date} from The start date of the lesson
  * @property {Date} to The end date of the lesson
  * @property {boolean} isDetention Indicates if the lesson is a detention
@@ -21,3 +21,65 @@
  * @property {boolean} [isCancelled] Indicates if the lesson is cancelled
  * @property {string} [color] The color of the lesson
  */
+
+/**
+ * @typedef {Object} Mark
+ * @property {string} title The title of the mark
+ * @property {number} [value] The value of the mark
+ * @property {number} scale Maximum value of the mark
+ * @property {number} [average] The average of the mark
+ * @property {number} coefficient The coefficient of the mark
+ * @property {number} min Lower bound of the average
+ * @property {number} max Upper bound of the average
+ * @property {Date} date Date of the mark
+ * @property {boolean} isAway Student was away
+ */
+
+/**
+ * @typedef {Object} MarksSubjectAverage
+ * @property {number} student The student's average
+ * @property {number} studentClass The class' average
+ * @property {number} min Lower bound of the average
+ * @property {number} max Upper bound of the average
+ */
+
+/**
+ * @typedef {Object} MarksSubject
+ * @property {string} name The name of the subject
+ * @property {MarksSubjectAverage} averages The average of the subject
+ * @property {string} color The color of the subject
+ * @property {Mark[]} marks The marks of the subject
+ */
+
+/**
+ * @typedef {Object} MarksAverage
+ * @property {number} student The student's average
+ * @property {number} studentClass The class' average
+ */
+
+/**
+ * @typedef {Object} Marks
+ * @property {MarksSubject[]} subjects The marks of the student
+ * @property {MarksAverage} averages General average of the student and the class
+ */
+
+const pronote = {
+  /**
+   * @type {Lesson[]}
+   */
+  lessons: [
+    {
+      from: new Date(1647846300000),
+      to: new Date(1647849600000),
+      isDetention: false,
+      hasDuplicate: false,
+      remoteLesson: false,
+      subject: 'Histoire',
+      teacher: "Rejeton d'hétaïre",
+      room: 'C423 HG',
+      isAway: false,
+      isCancelled: false,
+      color: '#84d494',
+    },
+  ],
+};
